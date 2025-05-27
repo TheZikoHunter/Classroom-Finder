a84143d181c55280f1f636c45f790861ae8d4df8
yassine lbarrah <elberrahy.1@gmail.com>
2025-05-27 17:26:09 +0100

    added prof interface

--- classroom-finder/ui/src/app/app.module.ts
+++ classroom-finder/ui/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { inject } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProfessorManagementComponent } from './component/professor-management/professor-management.component';
@@ -12,39 +12,60 @@ import { MajorManagementComponent } from './component/major-management/major-man
 import { ScheduleManagementComponent } from './component/schedule-management/schedule-management.component';
 import { EmptyClassroomComponent } from './component/empty-classroom/empty-classroom.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-import { DashboardComponent } from './component/dashbord/dashboard.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ProfessorTimetableComponent } from './component/professor-timetable/professor-timetable.component';
+import { LoginComponent } from './component/login/login.component';
+import { AuthService } from './auth/auth.service';
 
 const routes: Routes = [
-  { path: 'professors', component: ProfessorManagementComponent },
-  { path: 'subjects', component: SubjectManagementComponent },
-  { path: 'classrooms', component: ClassroomManagementComponent },
-  { path: 'majors', component: MajorManagementComponent },
-  { path: 'schedule', component: ScheduleManagementComponent },
-  { path: 'empty-classrooms', component: EmptyClassroomComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { 
+    path: 'professors', 
+    component: ProfessorManagementComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'subjects', 
+    component: SubjectManagementComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'classrooms', 
+    component: ClassroomManagementComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'majors', 
+    component: MajorManagementComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'schedule', 
+    component: ScheduleManagementComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'empty-classrooms', 
+    component: EmptyClassroomComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent,
+    canActivate: [() => inject(AuthService).isAdmin()]
+  },
+  { 
+    path: 'professor-timetable', 
+    component: ProfessorTimetableComponent,
+    canActivate: [() => inject(AuthService).isProfessor()]
+  }
 ];
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ProfessorManagementComponent,
-    SubjectManagementComponent,
-    ClassroomManagementComponent,
-    MajorManagementComponent,
-    ScheduleManagementComponent,
-    EmptyClassroomComponent,
-    NavbarComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations()
+  ]
+}).catch(err => console.error(err));
